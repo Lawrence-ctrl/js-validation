7f53cc6b745dbe6348fa49a5917f248ce24db77d
Thuta Yar Moe <thutayarmoe@Thutas-MacBook-Pro.local>
2022-01-22 21:47:55 +0630

    add validaton functions

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-function isEmail(val) {
-  if (val == 'email') return true
-  else return false
-})
+const validation = {
+  isEmail: (email) => {
+    let emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(String(email).toLowerCase().match(emailRegex)) return true;
+    else return false;
+  },
+
+  isMinString: (string, length) => {
+    if (string && string.length >= length) return true;
+    else return false;
+  },
+
+  isMaxString: (string, length) => {
+    if (string && string.length <= length) return true;
+    else return false;
+  }
+}
+
+module.exports = validation;
